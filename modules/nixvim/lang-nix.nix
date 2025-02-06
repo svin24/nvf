f836cd74d2a44590a42782ee0fd060caa7206e4e
anntnzrb <anntnzrb@proton.me>
2025-02-06 17:50:25 -0500

    nix: use nixd lsp

--- modules/nixvim/lang-nix.nix
+++ modules/nixvim/lang-nix.nix
@@ -5,9 +5,13 @@
 {
   config.vim.languages.nix = {
     enable = true;
+    lsp.server = "nixd";
     format = {
       package = pkgs.nixfmt-rfc-style;
       type = "nixfmt";
     };
   };
+
+  # FIXME: this should be bundled by the above; expecting upstream fix
+  config.vim.extraPackages = [ pkgs.nixd ];
 }
